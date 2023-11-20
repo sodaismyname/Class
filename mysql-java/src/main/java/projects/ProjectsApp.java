5134e0f0626d1e46738b7f3358c093ec510c8f37
sodaismyname <soda.mascolo@gmail.com>
2023-11-19 18:14:37 -0800

    added Week07 Coding assignment

---
+++ mysql-java/src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,16 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public ProjectsApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		DbConnection.getConnection();
+
+	}
+
+}
